ce8ca54d95a6c827c999ebd825cd661eb063fbe8
Tushar Sharma <sharma@aclanyc.com>
2017-02-27 13:21:41 +0000

    made cahnges

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//Write your function calls below this line
+setColor(0,0,"blue");
+setColor(9,0,"blue");
+setColor(0,9,"blue");
+setColor(9,9,"blue");
+setColor(3,3,"red");
+setColor(3,5,"red");
+setColor(5,2,"red");
+setColor(6,3,"red");
